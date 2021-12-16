585aeeceeb246dfb5452fec48e7280ba089a6e51
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-15 17:50:22 -0800

    added BR

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <NavLink to="/books">Books</NavLink>
+        <Switch>
+          <Route path="/books" component={BookList} />
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
