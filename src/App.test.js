0c823960d234ae1880540e48f69b70b0d48705d4
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-16 15:22:04 -0800

    added passing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-it('renders a welcome message when signed out', async () => {
-  const { container } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
-
-  await screen.findByRole('list', { name: 'book list' });
-
-  expect(container).toMatchSnapshot();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Books/i);
+  expect(linkElement).toBeInTheDocument();
 });
